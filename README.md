2c9be5491c7bc444dfc8e3ee96824e02165ff331
Shereen Finney <shereenfinney@gmail.com>
2017-01-17 16:52:21 +0000

    Created views to separate routing (app.rb) from presentation(index.erb)

--- README.md
+++ README.md
@@ -7,4 +7,21 @@
 
 ## To address **separation of concerns**
 * Separate our Ruby and HTML code by modularising our code into a **View**
-* app.rb will be for routing (or controlling) concerns and we will use another file for our presentation (or view) concerns.
+* app.rb will be for routing (or controlling) concerns and we will use views/index.erb for our presentation (or view) concerns.
+
+# app.rb
+```
+require 'sinatra'
+set :session_secret, 'super secret'
+get '/cat' do # - accessed via 'http://localhost:4567/cat'
+  erb(:index)
+end
+
+```
+# index.erb
+```
+"<div style='border: 4px dashed red'>
+  <img src='http://bit.ly/1eze8aE'>
+</div> "
+
+```
--- app.rb
+++ app.rb
@@ -1,3 +1,4 @@
+# This is the routing file - start the server with shotgun app.rb -p 4567
 require 'sinatra'
 set :session_secret, 'super secret'
 
@@ -16,18 +17,21 @@ get '/no3' do
   "This is number 3"
 end
 
-# We've created a web application that returns basic strings as responses to client requests.
-# Most web applications don't return basic strings: they return advanced HTML documents,
-# along with CSS (for styling), JavaScripts (for interaction effects), and other assets
-# (like images, videos, and audio). Let's now return some HTML!
-#4th route defined - run with shotgun app.rb -p 4567
+# We've created a web application that returns basic strings as responses to client requests. Most web applications don't return basic strings: they return advanced HTML documents,
+# along with CSS (for styling), JavaScripts (for interaction effects), and other assets (like images, videos, and audio). Let's now return some HTML!
 
-get '/cat' do # - accessed via 'http://localhost:4567/cat'
-  " <div style='border: 4px dashed red'>
-      <img src='http://bit.ly/1eze8aE'>
-    </div>  "
-end
+# 4th route defined - run with shotgun app.rb -p 4567
+
+# get '/cat' do # - accessed via 'http://localhost:4567/cat'
+#   " <div style='border: 4px dashed red'>
+#       <img src='http://bit.ly/1eze8aE'>
+#     </div>  "
+# end
 
 # Next up, we'll see if we can modularize some of our code into a view. Views are used to
 # separate Ruby and HTML code. app.rb wull be for routing (or controlling) concerns and
 # we will use another file for our presentation (or view) concerns.
+
+get '/cat' do # - accessed via 'http://localhost:4567/cat'
+  erb(:index)
+end
